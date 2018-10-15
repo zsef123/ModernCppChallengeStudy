1b56246ce3b73b55b875372ba3aa15fc96bfaf3c
zsef123 <zsef123@gmail.com>
2018-10-15 11:31:25 +0900

    Solved Math-Problem1

--- Math/Problem1/main.cpp
+++ Math/Problem1/main.cpp
@@ -1,6 +1,32 @@
 #include <gsl/gsl>
+/*
+* Write a program that calculates and prints
+* the sum of all the natural numbers divisible by either 3 or 5,
+* up to a given limit entered by the user.
+*/
+
+unsigned int div3or5(int input_) {
+    if (input_ == 3)
+        return 3;
+    
+    unsigned int output_;
+
+    for (unsigned int i = 0; i <= input_ ; i += 3)
+        output_ += i;
+    for (unsigned int i = 0; i <= input_ ; i += 5) 
+        output_ += i;
+    for (unsigned int i = 0; i <= input_ ; i += 15) 
+        output_ -= i;
+        
+    return output_;
+}
 
 int main(int argc, char* argv[])
-{
+{   
+    unsigned int input_;
+    printf("Input Numer : "); scanf("%d", &input_);
+
+    unsigned int output_ = div3or5(input_);
+    printf("Sum : %d\n", output_);
     return 0;
 }
