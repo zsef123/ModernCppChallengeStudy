f26c1b4568d025abe30ceffed362dde952ec367d
zsef123 <zsef123@gmail.com>
2018-10-16 21:35:58 +0900

    Solved Math-Problem14

--- Math/Problem14/main.cpp
+++ Math/Problem14/main.cpp
@@ -1,6 +1,23 @@
 #include <gsl/gsl>
 
+// Write a program that validates that 10-digit values entered by the user, as a string, represent valid ISBN-10 numbers.
+
+bool validISBN(char *isbn) {
+    int sum = 0;
+    for (int i = 0; i < 10; i++) 
+        sum += int(isbn[i] - '0')  * (i + 1);
+    return sum % 11;
+}
+
 int main(int argc, char* argv[])
 {
+    char input[11] = {0};
+    printf("Input string : "); scanf("%s", input);
+
+    bool flag = validISBN(input);
+    if (flag)
+        printf("ISBN True");
+    else
+        printf("ISBN False");
     return 0;
 }
