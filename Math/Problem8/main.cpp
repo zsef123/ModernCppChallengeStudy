2a8471ba9f90a39b8b46608d8297f6a024c4ca10
zsef123 <zsef123@gmail.com>
2018-10-19 23:07:46 +0900

    Solved Math-Problem8

--- Math/Problem8/main.cpp
+++ Math/Problem8/main.cpp
@@ -1,6 +1,32 @@
 #include <gsl/gsl>
+#include <iostream>
+#include <string>
+#include <math.h>
+#define uint uint32_t
+
+// Write a program that prints all Armstrong numbers with three digits.
+
+bool isArmstrong(int x) {
+    std::string digits = std::to_string(x);
+    int out = 0;
+    for (auto d : digits) {
+        // int i = std::stoi(d);
+        int i = d - '0';
+        out += uint(pow(i, digits.length()));
+    }
+    return (x == out);
+}
+
+void printArmstrong() {
+    for (int i = 100; i <= 999; i++) {
+        if (isArmstrong(i))
+            printf("%d ", i);
+    }
+    printf("\n");
+}
 
 int main(int argc, char* argv[])
 {
+    printArmstrong();
     return 0;
 }
