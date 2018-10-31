d3c319483ac403ea302c10aeae6827d438fd2810
zsef123 <zsef123@gmail.com>
2018-10-31 11:34:30 +0900

    Solved Problem22

--- Language/Problem22/main.cpp
+++ Language/Problem22/main.cpp
@@ -15,22 +15,22 @@ class Temperature {
     private:
         double cel, fah, kel;
     public:
-        Temperature(std::string input) {
+        Temperature(std::string& input) {
             size_t token = input.find("_");
             double x = stod(input.substr(0, token));
-            char type = input[token + 1].c_str()[0];
+            const char type = input[token + 1];
 
-            if (type == "d") {
+            if (type == 'd') {
                 cel = x;
                 fah = (x * 9/5) + 32;
                 kel = x + 273.15;
             }
-            else if (type == "f") {
+            else if (type == 'f') {
                 fah = x;
                 cel = (x - 32) * (5/9);
                 kel = cel + 273.15;
             }
-            else if (type == "K") {
+            else if (type == 'K') {
                 kel = x;                
                 cel = x - 273.15;
                 fah = (cel * 9/5) + 32;
@@ -41,12 +41,12 @@ class Temperature {
             printf("cel : %lf, fah : %lf, kel : %lf\n", cel, fah, kel);
         }
 
-}
+};
 
 int main(int argc, char* argv[])
 {
     std::string input;
-    std::cin << input;
+    std::cin >> input;
     Temperature t(input);
     t.printout();
     return 0;
