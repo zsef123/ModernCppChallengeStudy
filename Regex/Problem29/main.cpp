76268059a6015bc3063b2e8600b8eaea5cf63e2f
zsef123 <zsef123@gmail.com>
2018-11-02 20:04:42 +0900

    Solved Problem29

--- Regex/Problem29/main.cpp
+++ Regex/Problem29/main.cpp
@@ -1,6 +1,22 @@
-#include <gsl/gsl>
+#include <iostream>
+#include <regex>
+#include <string>
+// License plate validation
+
+void print_license(const std::string &s) {
+    std::regex r("[A-Z]{3}-[A-Z]{2} [0-9]{3}");
+    for(std::sregex_iterator i = std::sregex_iterator(s.begin(), s.end(), r);
+                             i != std::sregex_iterator();
+                             ++i )
+    {
+        std::smatch m = *i;
+        std::cout << m.str() << " at position " << m.position() << '\n';
+    }
+}
 
 int main(int argc, char* argv[])
 {
+    std::string input = "AZA-FE 012, vsdvvz, ZVZ-ZZ 000";
+    print_license(input);
     return 0;
 }
